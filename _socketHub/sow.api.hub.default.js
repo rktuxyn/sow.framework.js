36c49784af687e3dc658f8811489f272fb288eb6
RKTUXYN <mssclang@outlook.com>
2019-04-11 04:11:39 +0600

    Add files via upload

---
+++ _socketHub/sow.api.hub.default.js
@@ -0,0 +1,26 @@
+﻿/**
+* Copyright (c) 2018, SOW (https://www.facebook.com/safeonlineworld). (https://github.com/RKTUXYN) All rights reserved.
+* @author {SOW}
+* @description {sow.api.hub.default.js}
+* @example { }
+* Copyrights licensed under the New BSD License.
+* See the accompanying LICENSE file for terms.
+*/
+( function () {
+	Sow.usingNamespace( 'Sow.Net.Hub' );
+	Sow.hook( "Manager" ).add( "onSginOut", function ( a ) {
+		location.href = "/sginout.aspx?path=/&next=task";
+	} );
+	Sow.exportNamespace( 'Sow.Net.Hub' ).init( {
+		pingInterval: 300000,
+		waitForPageLoad: true,
+		jsonp: false,
+		withCredentials: true,
+		loging: false,
+		transport: 'webSockets'
+	} ).start().done( () => {
+		console.log( 'CONNECTED' );
+	} ).fail( () => {
+		console.log( 'NOT_CONNECTED' );
+	} );
+}() );
